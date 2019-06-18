f85e89e141f0d1cb66041626c93f0647b7490b97
Dmytro Sychevsky <sych@pdffiller.com>
2019-06-18 18:20:36 +0300

    fix json format

--- selenoid-exporter.php
+++ selenoid-exporter.php
@@ -11,10 +11,10 @@ $selenoid_status_url = getenv('SELENOID_STATUS_URL') ?: 'localhost:8080/status';
 $json     = json_decode(@file_get_contents($selenoid_status_url), true);
 $hostname = gethostname();
 
-if (empty($json) || empty($json['state'])) {
+if (empty($json) || !array($json)) {
     exit(0);
 }
 
-foreach (array_slice($json['state'], 0,4) as $key => $val){
+foreach (array_slice($json, 0,4) as $key => $val){
     echo 'selenoid_state_'.$key.'{hostname="'.$hostname.'"} '.$val.PHP_EOL;
 }
